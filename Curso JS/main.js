d91b881d8aae8e3aa6de36ddb7f8289c1a3b4f01
Diego Rodrigo <diers20@hotmail.com>
2020-03-07 10:31:03 -0300

    commit

---
+++ Curso JS/main.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET','https://api.github.com/users?since=135>; rel="next"');
+xhr.send(null);
+
+xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4){
+        console.log(JSON.parse(xhr.responseText));
+    }
+}
